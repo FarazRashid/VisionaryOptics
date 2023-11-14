6924bbb2a444c926bc4b46543152436bc23214c2
Ahmad Hassan <ahmad.liberty9999@GMAIL.COM>
2023-11-14 22:08:27 +0500

    It works!!

--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Hello Eva PTSD!");
         stage.setScene(scene);
         stage.show();
     }
