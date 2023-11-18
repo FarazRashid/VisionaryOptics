631249e6744bc103e41192fb445d1d890153f60b
Ahmad Hassan <ahmad.liberty9999@GMAIL.COM>
2023-11-18 23:58:57 +0500

    adds the chat bot

--- demo/src/main/java/com/example/demo/ReceiveCustomerSupportController.java
+++ demo/src/main/java/com/example/demo/ReceiveCustomerSupportController.java
@@ -46,98 +46,58 @@ public class ReceiveCustomerSupportController {
     }
 
     void initiate(){
-        String initialMessage = "You are now an employee of Visionary Optics and you will respond to the queries of the customers\n" +
-                "\n" +
-                "\n" +
-                "Sure, I'd be happy to assist you. Please provide your order number.\n" +
-                "I apologize for the inconvenience. Let me check the status for you.\n" +
-                "Yes, you can initiate a return within 30 days of receiving them.\n" +
-                "You can track your order using the tracking number in your email.\n" +
-                "Model A has a metal frame, while model B has a lightweight plastic frame.\n" +
-                "Yes, we provide prescription lenses. Please specify your prescription details.\n" +
-                "You can reach us at support@visionaryoptics.com or call +92-VISIONARY-EYES.\n" +
-                "Check our website for the latest promotions and discounts.\n" +
-                "\n" +
-                "Our eyewear is designed for both style and comfort.\n" +
-                "We offer a variety of lens coatings for UV protection and anti-glare.\n" +
-                "We have a wide range of frame styles to suit different preferences.\n" +
-                "Orders typically ship within 1-2 business days after confirmation.\n" +
-                "For returns, please ensure the eyewear is in its original condition.\n" +
-                "Our customer support is available Monday to Friday, 9 AM - 5 PM (GMT).\n" +
-                "Explore our virtual try-on feature to see how frames look on you.\n" +
-                "Sign up for our newsletter to receive exclusive offers and updates.\n" +
-                "Please provide your feedback on our products and services.\n" +
-                "\n" +
-                "Custom prescriptions can be added during the online ordering process.\n" +
-                "We offer a 1-year warranty on manufacturing defects for our eyewear.\n" +
-                "Our packaging is eco-friendly, reflecting our commitment to sustainability.\n" +
-                "Lost your tracking number? Check your order confirmation email.\n" +
-                "For international orders, shipping times may vary based on location.\n" +
-                "Our frames are made from high-quality materials for durability.\n" +
-                "Check our FAQs page for answers to common questions.\n" +
-                "Follow us on social media for the latest eyewear trends and updates.\n" +
-                "Need assistance with a specific issue? Describe it, and we'll help you out.";
-        // create a chat room between chatGPT api and user
-        String response = chatGPT(initialMessage);
-        chatTextArea.appendText("ChatGPT: " + response + "\n");
-        // send a message to chatGPT api
-
-        // receive a message from chatGPT api
-
-
 
     }
     @FXML
     void onSendMessage(ActionEvent event) {
+
         String message = messageTextField.getText();
         chatTextArea.appendText("You: " + message + "\n");
         messageTextField.clear();
-        String response = chatGPT(message);
-        chatTextArea.appendText("ChatGPT: " + response + "\n");
+        String response = getResponse(message);
+        chatTextArea.appendText("Support: " + response + "\n");
 
     }
 
-    public static String chatGPT(String conversation) {
-        String url = "https://api.openai.com/v1/chat/completions";
-        String apiKey = "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";
-        String model = "gpt-3.5-turbo";
+    public static String getResponse(String userQuery) {
+        String response;
 
-        try {
-            // Create the HTTP POST request
-            URL obj = new URL(url);
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-            con.setRequestMethod("POST");
-            con.setRequestProperty("Authorization", "Bearer " + apiKey);
-            con.setRequestProperty("Content-Type", "application/json");
+        String lowerCaseQuery = userQuery.toLowerCase();
 
-            // Build the request body with conversation
-            String body = String.format(
-                    "{\"model\": \"%s\", \"messages\": %s, \"temperature\": 0.7}",
-                    model, conversation);
-
-            con.setDoOutput(true);
-            OutputStreamWriter writer = new OutputStreamWriter(con.getOutputStream());
-            writer.write(body);
-            writer.flush();
-            writer.close();
-
-            // Get the response
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuilder response = new StringBuilder();
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-
-            // Return the extracted contents of the response.
-            return response.toString();
-
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, "Failed to connect to the server.", e);
-            return "Sorry, I'm having trouble connecting to the server.";
+        if (lowerCaseQuery.contains("order") && !lowerCaseQuery.contains("track")) {
+            response = "Sure, I'd be happy to assist you. Please provide your order number.";
+        } else if (lowerCaseQuery.contains("check status")) {
+            response = "I apologize for the inconvenience. Let me check the status for you.";
+        } else if (lowerCaseQuery.contains("return")) {
+            response = "We offer a 1-year warranty on manufacturing defects for our eyewear. If you encounter any issues related to the manufacturing of your glasses within the first year of purchase, please contact us with your order number and a description of the problem. We'll be happy to assist you in resolving the issue or, if necessary, provide a replacement.";
+        } else if (lowerCaseQuery.contains("track")) {
+            response = "You can track your order using the tracking number in your email.";
+        } else if (lowerCaseQuery.contains("frame")) {
+            response = "We provide light-weight metal and plastic frames.";
+        } else if (lowerCaseQuery.contains("prescription lenses")) {
+            response = "Yes, we provide prescription lenses. You can find out more about them at our website!";
+        } else if (lowerCaseQuery.contains("contact")) {
+            response = "You can reach us at support@visionaryoptics.com or call +92-VISIONARY-EYES.";
+        } else if (lowerCaseQuery.contains("promotions") || lowerCaseQuery.contains("discounts")) {
+            response = "Check our website for the latest promotions and discounts.";
+        } else if (lowerCaseQuery.contains("design")) {
+            response = "Our eye-wear is designed for both style and comfort.";
         }
+        else if (lowerCaseQuery.contains("hi") || lowerCaseQuery.contains("hello")){
+            response = "How may I help you today?";
+        }
+        else if (lowerCaseQuery.contains("bye")){
+            response = "Thank you for contacting Visionary Optics Support. Have a nice day!";
+        }
+        else {
+            response = "I'm sorry, I didn't understand that. Please provide a valid query.";
+        }
+
+        return response;
     }
+
+
+
 
     // This method extracts the response expected from chatGPT and returns it.
     public static String extractContentFromResponse(String response) {
